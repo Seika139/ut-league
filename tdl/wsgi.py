5f5206404ad7d16033b51486009f0b1fb1ae8b44
Seika139 <tcoanzu@gmail.com>
2019-04-23 20:46:41 +0900

    Herokuのサーバーが30分以上アクセスがないのをストップする

--- tdl/wsgi.py
+++ tdl/wsgi.py
@@ -8,9 +8,22 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
-
 from dj_static import Cling
 from django.core.wsgi import get_wsgi_application
+import threading, requests, time
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tdl.settings")
 application = Cling(get_wsgi_application())
+
+def awake():
+    while 1:
+        try:
+            print("Start Awaking")
+            requests.get("http://toudai-league.herokuapp.com/")
+            print("End")
+        except:
+            print("error")
+        time.sleep(60*10)
+
+t = threading.Thread(target=awake)
+t.start()
